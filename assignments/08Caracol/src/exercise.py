f8767505f96c0827fba6cb6f80ab44f132b762bf
Mike-GB <88850952+Mike-GB@users.noreply.github.com>
2021-08-21 17:27:45 -0500

    Update exercise.py

--- assignments/08Caracol/src/exercise.py
+++ assignments/08Caracol/src/exercise.py
@@ -1,8 +1,8 @@
 def main():
     #escribe tu código abajo de esta línea
-    minutos = int(input("Dame los minutos: "))
-    velocidad = 5.7
-    centímetros_recorridos = (minutos*velocidad)*6
+    minutos = float(input("Dame los minutos: "))
+    velocidad = 5.7*6
+    centímetros_recorridos = (minutos*velocidad)
     print("Centímetros recorridos:", centímetros_recorridos)
 
 if __name__ == '__main__':
