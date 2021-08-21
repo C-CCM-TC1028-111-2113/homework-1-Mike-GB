d337d5cd4b6aff8a1ef213bc1e5b8c3f8d81e7e8
Mike-GB <88850952+Mike-GB@users.noreply.github.com>
2021-08-21 16:10:42 -0500

    Update exercise.py

--- assignments/07PendienteLinea/src/exercise.py
+++ assignments/07PendienteLinea/src/exercise.py
@@ -5,7 +5,7 @@ y1=float(input("Dame y1"))
 x2=float(input("Dame x2"))
 y2=float(input("Dame y2"))
 m=float((y2-y1)/(x2-x1))
-print("Pendiente: "+str(m))
+print("Pendiente: ",m)
     #Lee los datos
     pass
 
